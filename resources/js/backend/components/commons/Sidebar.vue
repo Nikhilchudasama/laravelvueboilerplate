73f729257a294c0120abaeddae4b3dde47521155
Nikhilchudasama <nikhilgujcet@gmail.com>
2020-02-20 19:28:07 +0530

    fixing file

---
+++ resources/js/backend/components/commons/Sidebar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="sidebar">
+    <nav class="sidebar-nav">
+      <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link" href>
+            <i class="nav-icon fas fa-tachometer-alt"></i>
+            Dashboard
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Sidebar"
+}
+</script>
